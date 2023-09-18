ad9ac85e61e318ece710406fc49e4e6be359ff66
Sachin Kumar <96615888+sachin-shaw@users.noreply.github.com>
2023-09-19 05:24:08 +0530

    Add files via upload

---
+++ Dijkstra-algo.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int findMinVertex(int *distance, bool *visited, int n)
+{
+    int minVertex = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i] && (minVertex == -1 || distance[i] < distance[minVertex]))
+        {
+            minVertex = i;
+        }
+    }
+    return minVertex;
+}
+
+void dijkstra(int **edges, int n)
+{
+    int *distance = new int[n];
+    bool *visited = new bool[n];
+
+    for (int i = 1; i < n; i++)
+    {
+        distance[i] = INT_MAX;
+        visited[i] = false;
+    }
+    distance[0] = 0;
+    visited[0] = false;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minVertex = findMinVertex(distance, visited, n);
+        visited[minVertex] = true;
+
+        for (int j = 0; j < n; j++)
+        {
+            if (edges[minVertex][j] != 0 && !visited[j])
+            {
+                int dist = distance[minVertex] + edges[minVertex][j];
+                if (dist < distance[j])
+                {
+                    distance[j] = dist;
+                }
+            }
+        }
+    }
+
+    cout << "answer" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << " " << distance[i] << endl;
+    }
+
+    delete[] visited;
+    delete[] distance;
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int **edges = new int *[n];
+    for (int i = 0; i < n; i++)
+    {
+        edges[i] = new int[n];
+        for (int j = 0; j < n; j++)
+        {
+            edges[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < e; i++)
+    {
+        int f, s, w;
+        cin >> f >> s >> w;
+        edges[f][s] = w;
+        edges[s][f] = w;
+    }
+
+    dijkstra(edges, n);
+}
+
+/*
+5 7
+0 1 4
+0 2 8
+1 3 5
+1 2 2
+2 3 5
+2 4 9
+3 4 4
+*/
