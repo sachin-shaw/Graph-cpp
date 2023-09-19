4c6ea0611ecd4ab4e1399a28f26f5084e3012e39
Sachin Kumar <96615888+sachin-shaw@users.noreply.github.com>
2023-09-20 03:51:27 +0530

    Create DFS-algo.cpp

---
+++ DFS-algo.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void DFSUtil(int i, vector<int> adj[], int V, bool vis[],
+                 vector<int> &res) {
+        if (vis[i]) return;
+
+        // marking vertex as visited and adding it to output list.
+        vis[i] = true;
+        res.push_back(i);
+
+        // iterating over connected components of the vertex and if any
+        // of them is not visited then calling the function recursively.
+        for (int j : adj[i]) {
+            if (!vis[j]) DFSUtil(j, adj, V, vis, res);
+        }
+    }
+
+    // Function to return a list containing the DFS traversal of the graph.
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        // using a boolean list to mark all the vertices as not visited.
+        bool vis[V];
+        memset(vis, false, sizeof(vis));
+
+        vector<int> res;
+        for (int i = 0; i < V; i++) {
+            // if any vertex isn't visited then calling the function.
+            if (!vis[i]) {
+                DFSUtil(i, adj, V, vis, res);
+            }
+        }
+        // returning the output list.
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.dfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
